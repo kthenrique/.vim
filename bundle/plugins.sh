f01052f4416520711bdc70622de927333999848b
Kelve T. Henrique <kelvehenrique@hotmail.com>
2018-08-24 01:15:49 +0200

    Update script for plugins

--- bundle/plugins.sh
+++ bundle/plugins.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
 # OmniCppComplete
-git clone https://github.com/vim-scripts/OmniCppComplete.git
+git clone https://github.com/vim-scripts/OmniCppComplete.git ~/.vim/bundle/
 
 # Ctrl-P
-git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/
 
 # DragVisuals
-git clone https://github.com/shinokada/dragvisuals.vim.git
+git clone https://github.com/shinokada/dragvisuals.vim.git ~/.vim/bundle/
 
 # Jedi-Vim
-git clone https://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/davidhalter/jedi-vim.git ~/.vim/bundle/
 
 # Lightline
-git clone https://github.com/itchyny/lightline.vim.git
+git clone https://github.com/itchyny/lightline.vim.git ~/.vim/bundle/
 
 # SuperTab
-git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/
 
 # Syntastic
-git clone https://github.com/vim-syntastic/syntastic.git
+git clone https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/
 
 # TagBar
-git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/
 
 # Vim-GutenTags
-git clone https://github.com/ludovicchabant/vim-gutentags.git
+git clone https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/
 
 # Vim-Latex-Live_Preview
-git clone https://github.com/xuhdev/vim-latex-live-preview.git
+git clone https://github.com/xuhdev/vim-latex-live-preview.git ~/.vim/bundle/
 
 # Vim-Tmux-Navigator
-git clone https://github.com/christoomey/vim-tmux-navigator.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/bundle/
 
