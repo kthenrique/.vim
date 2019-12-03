b5f4cde38261f7c815c5fbecca5ce0406ea4060f
Kelve T. Henrique <kelvehenrique@pm.me>
2019-12-03 22:48:54 +0100

    Add skeletons for java & cpp

---
+++ skeleton/skeleton_doxy.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file     "_______".cpp
+ * @version  VX.X
+ * @date     Mmm YYYY
+ * @author   Kelve T. Henrique
+ *
+ * @brief    < What is the purpose of this C file >
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    return 0;
+}
---
+++ skeleton/skeleton_doxy.java
@@ -0,0 +1,15 @@
+/**
+ * @file     "_______".java
+ * @version  VX.X
+ * @date     Mmm YYYY
+ * @author   Kelve T. Henrique
+ *
+ * @brief    < What is the purpose of this C file >
+ *
+ */
+
+public class <filename>{
+
+    public static void main(String[] argv) {
+    }
+}
