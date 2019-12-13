07a60eeb1eaa313fcaebd49cb710900f1ff4a45c
Kelve T. Henrique <kelvehenrique@pm.me>
2019-12-13 18:39:51 +0100

    Add java skeleton with javadoc

--- skeleton/skeleton_doxy.java
+++ skeleton/skeleton_doxy.java
@@ -1,15 +1,42 @@
 /**
- * @file     "_______".java
+ * file     "_______".java
  * @version  VX.X
- * @date     Mmm YYYY
+ * date     Mmm YYYY
  * @author   Kelve T. Henrique
  *
- * @brief    < What is the purpose of this C file >
+ * brief    < What is the purpose of this java file >
  *
  */
 
+/**
+ * SOME CLASS
+ * <p>
+ * Short description
+ * <p>
+ * Long description
+ */
 public class <filename>{
 
+    /**
+     * TITLE
+     * <p>
+     * short description
+     * <p>
+     * long description
+     *
+     * @param arg describe it
+     * @return describe it
+     */
+    int callMe(char arg);
+
+    /**
+     * THE LEGENDARY MAIN FUNCTION
+     * <p>
+     * Discards any comments
+     * <p>
+     *
+     * @param argv The String array of user arguments
+     */
     public static void main(String[] argv) {
     }
 }
